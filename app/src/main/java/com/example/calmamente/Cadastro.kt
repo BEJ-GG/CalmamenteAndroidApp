a57d1f1ff1c67c12d287a2b4d614b1980677dc8f
Guilherme-Beck-Fiap <rm87531@fiap.com.br>
2021-09-15 16:11:13 -0300

    commit2

--- app/src/main/java/com/example/calmamente/Cadastro.kt
+++ app/src/main/java/com/example/calmamente/Cadastro.kt
@@ -1,11 +1,29 @@
 package com.example.calmamente
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 
 class Cadastro : AppCompatActivity() {
+
+    lateinit var spinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+
+        spinner = findViewById(R.id.spinner)
+        var gen = arrayOf("Masculino", "Feminino", "Não-binário")
+
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, gen)
+
+        fun onChatbot(v: View?) {
+
+            val intent = Intent(this, ChatBot::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/calmamente/ChatBot.kt
@@ -0,0 +1,11 @@
+package com.example.calmamente
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatBot : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_bot)
+    }
+}
